dc7618f55881e20d628917259ac967d72b31b313
Liam Ostrander <119699311+figgythenewton@users.noreply.github.com>
2025-01-28 16:53:31 -0500

    added custom pixel art bg

--- Luna/Source/PluginEditor.cpp
+++ Luna/Source/PluginEditor.cpp
@@ -17,7 +17,7 @@ LunaAudioProcessorEditor::LunaAudioProcessorEditor (LunaAudioProcessor& p)
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     //auto sadMickey = juce::ImageCache::getFromMemory(BinaryData::sad_mickey_png, BinaryData::sad_mickey_pngSize);
-    auto sadMickey = juce::ImageCache::getFromMemory(BinaryData::LunaPluginBG_png, BinaryData::LunaPluginBG_pngSize);
+    auto sadMickey = juce::ImageCache::getFromMemory(BinaryData::luna_ui_bg_800_600_1_png, BinaryData::luna_ui_bg_800_600_1_pngSize);
     
     if (! sadMickey.isNull())
         mImageComponent.setImage (sadMickey, juce::RectanglePlacement::stretchToFit);
